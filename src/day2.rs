bbc425c4fafa501cfaa5bd4792d4a9b17d247156
Owen O'Malley <omalley@apache.org>
2025-12-02 10:01:17 -0800

    added generic for shared code

--- src/day2.rs
+++ src/day2.rs
@@ -1,38 +1,46 @@
 use std::ops::RangeInclusive;
 use itertools::Itertools;
+use lazy_static::lazy_static;
 use num_integer::Integer;
 
-fn parse_int(s: &str) -> Result<i64, String> {
+type Product = u64;
+
+fn parse_int(s: &str) -> Result<Product, String> {
   s.parse().map_err(|_| format!("Can't parse integer - '{s}'"))
 }
 
-fn parse_line(s: &str) -> Result<RangeInclusive<i64>, String> {
+fn parse_line(s: &str) -> Result<RangeInclusive<Product>, String> {
   let (left, right) = s.trim().split_once('-')
       .ok_or("Can't parse range '{s}'")?;
   Ok(RangeInclusive::new(parse_int(left)?, parse_int(right)?))
 }
 
-pub fn generator(input: &str) -> Vec<RangeInclusive<i64>> {
+pub fn generator(input: &str) -> Vec<RangeInclusive<Product>> {
   input.split(',').map(parse_line).try_collect().expect("Can't parse input")
 }
 
-fn is_invalid(num: i64) -> bool {
+fn is_invalid(num: Product) -> bool {
   if num > 10 {
     let digits = num.ilog10() + 1;
     if digits.is_even() {
-      let split = 10_i64.pow(digits / 2);
+      let split = (10 as Product).pow(digits / 2);
       return (num % split) == (num / split)
     }
   }
   false
 }
 
-pub fn part1(input: &[RangeInclusive<i64>]) -> i64 {
-  input.iter().flat_map(|r| r.clone()).filter(|r| is_invalid(*r)).sum()
+fn sum_matching<F>(input: &[RangeInclusive<Product>], filter: F) -> Product
+    where F: Fn(Product) -> bool {
+  input.iter().flat_map(|r| r.clone()).filter(|r| filter(*r)).sum()
+}
+
+pub fn part1(input: &[RangeInclusive<Product>]) -> Product {
+  sum_matching(input, is_invalid)
 }
 
 /// Does the given number repeat digits given the power of 10 in split?
-fn digits_repeat(num: i64, split: i64) -> bool {
+fn digits_repeat(num: Product, split: Product) -> bool {
   let goal = num % split;
   let mut remainder = num / split;
   while remainder != 0 {
@@ -44,11 +52,11 @@ fn digits_repeat(num: i64, split: i64) -> bool {
   true
 }
 
-fn is_invalid2(num: i64) -> bool {
+fn is_invalid2(num: Product) -> bool {
   if num > 10 {
     let digits = num.ilog10() + 1;
     for part_digit in (1..=(digits/2)).rev() {
-      if digits.is_multiple_of(part_digit) && digits_repeat(num, 10_i64.pow(part_digit)) {
+      if digits.is_multiple_of(part_digit) && digits_repeat(num, (10 as Product).pow(part_digit)) {
         return true;
       }
     }
@@ -56,8 +64,8 @@ fn is_invalid2(num: i64) -> bool {
   false
 }
 
-pub fn part2(input: &[RangeInclusive<i64>]) -> i64 {
-  input.iter().flat_map(|r| r.clone()).filter(|r| is_invalid2(*r)).sum()
+pub fn part2(input: &[RangeInclusive<Product>]) -> Product {
+  sum_matching(input, is_invalid2)
 }
 
 #[cfg(test)]
