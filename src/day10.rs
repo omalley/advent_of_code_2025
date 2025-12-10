aef4c91c95f5df66b9b32e5e6518665496d55180
Owen O'Malley <omalley@apache.org>
2025-12-10 10:32:41 -0800

    wip

--- src/day10.rs
+++ src/day10.rs
@@ -1,28 +1,24 @@
-use std::collections::VecDeque;
 use std::vec::Vec;
 use itertools::Itertools;
-use smallvec::{smallvec, SmallVec};
+use smallvec::SmallVec;
 
 type LightMask = SmallVec<[bool; 16]>;
+type Button = SmallVec<[usize; 8]>;
 
 #[derive(Debug)]
 pub struct Machine {
   goal: LightMask,
-  buttons: Vec<LightMask>,
+  buttons: Vec<Button>,
   energy: Vec<usize>,
 }
 
 impl Machine {
-  fn parse_button(phrase: &str, bits: usize) -> Result<LightMask, String> {
+  fn parse_button(phrase: &str) -> Result<Button, String> {
     let phrase = phrase.strip_prefix("(")
         .ok_or(format!("Missing paren: '{phrase}'"))?;
     let phrase = phrase.strip_suffix(")")
         .ok_or(format!("Missing paren: '{phrase}'"))?;
-    let mut result = smallvec![false; bits];
-    for num in phrase.split(',').map(parse_int).collect::<Result<Vec<_>, _>>()? {
-      result[num] = true;
-    }
-    Ok(result)
+    phrase.split(',').map(parse_int).collect::<Result<Button, _>>()
   }
 
   fn parse_enery(phrase: &str) -> Result<Vec<usize>, String> {
@@ -40,15 +36,19 @@ impl Machine {
         .map(|c| c == '#')
         .collect();
     let mut buttons = button_str.split(" ")
-        .map(|s| Self::parse_button(s, goal_str.len() - 1))
+        .map(Self::parse_button)
         .collect::<Result<Vec<_>, _>>()?;
     buttons.sort_unstable();
     let energy = Self::parse_enery(energy_str)?;
     Ok(Machine{goal, buttons, energy})
   }
 
-  fn to_mask(light_mask: &LightMask) -> u64 {
+  fn lights_to_mask(light_mask: &LightMask) -> u64 {
     light_mask.iter().fold(0, |acc, &b| acc * 2 + if b {1} else {0})
+  }
+
+  fn button_to_mask(button: &Button, bits: usize) -> u64 {
+    button.iter().map(|b| 2_u64.pow((bits - *b - 1) as u32)).sum()
   }
 }
 
@@ -60,9 +60,11 @@ pub fn generator(input: &str) -> Vec<Machine> {
   input.lines().map(Machine::parse).collect::<Result<Vec<_>, _>>().expect("Bad input")
 }
 
-fn find_part1(machine: &Machine) -> usize {
-  let goal = Machine::to_mask(&machine.goal);
-  let buttons : Vec<u64> = machine.buttons.iter().map(|b| Machine::to_mask(b)).collect();
+fn solve_part1(machine: &Machine) -> usize {
+  let goal = Machine::lights_to_mask(&machine.goal);
+  let buttons : Vec<u64> = machine.buttons.iter()
+      .map(|b| Machine::button_to_mask(b, machine.goal.len()))
+      .collect();
   for pushes in 0..buttons.len() {
     for combo in buttons.iter().combinations(pushes) {
       if goal == combo.iter().fold(0, |acc, &b| acc ^ b) {
@@ -74,12 +76,41 @@ fn find_part1(machine: &Machine) -> usize {
 }
 
 pub fn part1(input: &[Machine]) -> usize {
-  input.iter().map(find_part1).sum()
+  input.iter().map(solve_part1).sum()
 }
 
+fn solve_part2_by_button(goal: &Vec<usize>, buttons: &[Button], pushes: usize) -> bool {
+  if pushes == 0 || buttons.is_empty() {
+    goal.iter().all(|x| *x == 0)
+  } else {
+    let max_pushes = pushes.min(buttons[0].iter().map(|i| goal[*i]).min().unwrap());
+    for our_pushes in (0..=max_pushes).rev() {
+      let mut sub_goal = goal.clone();
+      for b in &buttons[0] {
+        sub_goal[*b] -= our_pushes;
+      }
+      if solve_part2_by_button(&sub_goal, &buttons[1..], pushes - our_pushes) {
+        return true;
+      }
+    }
+    false
+  }
+}
+
+fn solve_part2(machine: &Machine) -> usize {
+  println!("Starting {machine:?}");
+  // we need at least the minimum of the energy and no worse than the sum.
+  for pushes in *(machine.energy.iter().min().unwrap())..=
+      machine.energy.iter().sum::<usize>() {
+    if solve_part2_by_button(&machine.energy, &machine.buttons, pushes) {
+      return pushes;
+    }
+  }
+  panic!("No solution found");
+}
 
 pub fn part2(input: &[Machine]) -> usize {
-  0
+  input.iter().map(solve_part2).sum()
 }
 
 #[cfg(test)]
@@ -101,6 +132,6 @@ mod tests {
   #[test]
   fn test_part2() {
     let data = generator(INPUT);
-    assert_eq!(0, part2(&data));
+    assert_eq!(33, part2(&data));
   }
 }
