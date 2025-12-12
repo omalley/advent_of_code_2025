2a31ff8d0afb11df149f66157b62bacf7de7edef
Owen O'Malley <omalley@apache.org>
2025-12-11 20:33:23 -0800

    day 11 add toposort.

--- src/day11.rs
+++ src/day11.rs
@@ -13,6 +13,7 @@ pub struct NamePair<'a> {
 pub struct Reactor<'a> {
   names: Vec<NamePair<'a>>,
   nodes: Vec<Node>,
+  toposort: Vec<usize>,
 }
 
 impl<'a> Reactor<'a> {
@@ -21,8 +22,30 @@ impl<'a> Reactor<'a> {
   }
 }
 
-const PART1_START_NAME: &str = "you";
 const OUT_NAME: &str = "out";
+
+fn toposort_visit(result: &mut Vec<usize>, node: usize, nodes: &[Node],
+                  visited: &mut [bool]) {
+  for child in &nodes[node].children {
+    if *child < visited.len() && !visited[*child] {
+      toposort_visit(result, *child, nodes, visited);
+    }
+  }
+  visited[node] = true;
+  result.push(node);
+}
+
+fn find_root_nodes(nodes: &[Node]) -> Vec<usize> {
+  let mut is_root = vec![true; nodes.len()];
+  for n in nodes.iter().flat_map(|n| n.children.iter()) {
+    if *n < is_root.len() {
+      is_root[*n] = false;
+    }
+  }
+  is_root.iter().enumerate()
+      .filter_map(|(i,e)| if *e { Some(i) } else { None })
+      .collect()
+}
 
 pub fn generator(input: &str) -> Reactor<'_> {
   let mut nodes = Vec::new();
@@ -40,18 +63,29 @@ pub fn generator(input: &str) -> Reactor<'_> {
     nodes[id].children.extend(child_str.split_whitespace()
         .map(|c| names[names.binary_search_by_key(&c, |n| n.name).unwrap()].id));
   }
-  Reactor{names, nodes}
+  // Generate a toposort.
+  let mut visited = vec![false; nodes.len()];
+  let mut toposort = Vec::new();
+  for root in find_root_nodes(&nodes) {
+    toposort_visit(&mut toposort, root, &nodes, &mut visited);
+  }
+  toposort.reverse();
+  Reactor{names, nodes, toposort}
 }
+
+const PART1_START_NAME: &str = "you";
 
 pub fn part1(input: &Reactor) -> usize {
   let start = input.find_name(PART1_START_NAME);
   let mut streams: Vec<usize> = vec![0; input.names.len()];
   streams[start] = 1;
-  while let Some(i) = streams.iter().position(|s| *s > 0) && i < input.nodes.len() {
-    let new_streams = streams[i];
-    streams[i] = 0;
-    for child in &input.nodes[i].children {
-      streams[*child] += new_streams;
+  for n in &input.toposort[start..] {
+    if *n < streams.len() && streams[*n] > 0 {
+      let new_streams = streams[*n];
+      streams[*n] = 0;
+      for child in &input.nodes[*n].children {
+        streams[*child] += new_streams;
+      }
     }
   }
   *streams.last().unwrap()
@@ -86,23 +120,24 @@ pub fn part2(input: &Reactor) -> usize {
   let fft = input.find_name("fft");
   let mut streams: Vec<StreamCounts> = vec![StreamCounts::default(); input.names.len()];
   streams[start] = StreamCounts{orig: 1, ..Default::default()};
-  while let Some(i) = streams.iter().position(|s| !s.is_empty())
-      && i < input.nodes.len() {
-    let mut new_streams = streams[i].clone();
-    if i == dac {
-      new_streams.dac += new_streams.orig;
-      new_streams.both += new_streams.fft;
-      new_streams.orig = 0;
-      new_streams.fft = 0;
-    } else if i == fft {
-      new_streams.fft += new_streams.orig;
-      new_streams.both += new_streams.dac;
-      new_streams.orig = 0;
-      new_streams.dac = 0;
-    }
-    streams[i] = StreamCounts::default();
-    for child in &input.nodes[i].children {
-      streams[*child].add(&new_streams);
+  for n in &input.toposort[start..] {
+    if *n < streams.len() && !streams[*n].is_empty() {
+      let mut new_streams = streams[*n].clone();
+      if *n == dac {
+        new_streams.dac += new_streams.orig;
+        new_streams.both += new_streams.fft;
+        new_streams.orig = 0;
+        new_streams.fft = 0;
+      } else if *n == fft {
+        new_streams.fft += new_streams.orig;
+        new_streams.both += new_streams.dac;
+        new_streams.orig = 0;
+        new_streams.dac = 0;
+      }
+      streams[*n] = StreamCounts::default();
+      for child in &input.nodes[*n].children {
+        streams[*child].add(&new_streams);
+      }
     }
   }
   streams.last().unwrap().both
---
+++ src/day12.rs
@@ -0,0 +1,144 @@
+use core::array;
+use lazy_static::lazy_static;
+use smallvec::SmallVec;
+use std::ops::RangeInclusive;
+
+type ProductId = u64;
+
+lazy_static! {
+  /// Generate a table of the powers of 10.
+  static ref POWER_10: [ProductId; 18] =
+    array::from_fn(|i| (10 as ProductId).pow(i as u32));
+}
+
+fn parse_int(s: &str) -> Result<ProductId, String> {
+  s.parse().map_err(|_| format!("Can't parse integer - '{s}'"))
+}
+
+#[derive(Clone,Debug)]
+pub struct RangeSlice {
+  range: RangeInclusive<ProductId>,
+  digits: usize,
+}
+
+type RangeSmallVec = SmallVec<[RangeSlice; 2]>;
+
+/// Parse a given range in to our internal form.
+/// We split the ranges in to sub-ranges that have consistent number of digits.
+/// Since 0 is annoying for ilog10, I forced 1 as a lower bound.
+fn parse_range(s: &str) -> Result<RangeSmallVec, String> {
+  let (left, right) = s.trim().split_once('-')
+      .ok_or("Can't parse range '{s}'")?;
+  let start = parse_int(left)?.max(1);
+  let end = parse_int(right)?.max(1);
+  let mut result: RangeSmallVec = SmallVec::new();
+  let start_digits = start.ilog10() as usize + 1;
+  let end_digits = end.ilog10() as usize + 1;
+  // Is the whole range a single number of digits?
+  if start_digits == end_digits {
+    result.push(RangeSlice{range: start..=end, digits: start_digits});
+  } else {
+    // Add the range for the start_digits.
+    result.push(RangeSlice{range: start..=(POWER_10[start_digits] - 1),
+      digits: start_digits});
+    // Add the middle ranges
+    for digits in (start_digits+1)..end_digits {
+      result.push(RangeSlice{range: POWER_10[digits-1]..=(POWER_10[digits] - 1),
+        digits});
+    }
+    // Add the range for the end_digits
+    result.push(RangeSlice{range: POWER_10[end_digits-1]..=end, digits: end_digits});
+  }
+  Ok(result)
+}
+
+/// Convert the input into a list of RangeSlices.
+pub fn generator(input: &str) -> Vec<RangeSlice> {
+  input.split(',').map(parse_range)
+      .collect::<Result<Vec<RangeSmallVec>,String>>()
+      .expect("Can't parse input")
+      .iter()
+      .flat_map(|rsv| rsv.iter())
+      .cloned()
+      .collect()
+}
+
+/// Given a split power of 10 and a number of repetitions, make the
+/// matching expanded mask.
+/// create_expanded_mask(100, 2) = 101
+/// create_expanded_mask(1000, 3) = 1001001
+fn create_expanded_mask(split: ProductId, repetitions: usize) -> ProductId {
+  let mut result = split + 1;
+  for _ in 2..repetitions {
+    result = result * split + 1;
+  }
+  result
+}
+
+/// Does the given num of length digits have a repeating pattern?
+/// We need to ignore prefixes with repeating patterns, because they will be counted
+/// with the smaller pattern.
+fn has_repeats(num: ProductId, digits: usize) -> bool {
+  for part_digits in 1..=(digits/2) {
+    if digits.is_multiple_of(part_digits) &&
+        num == (num / POWER_10[digits - part_digits]) *
+            create_expanded_mask(POWER_10[part_digits], digits / part_digits) {
+      return true;
+    }
+  }
+  false
+}
+
+fn count_invalid<F, const KEEP_DUPS: bool>(ranges: &[RangeSlice], split_func: F) -> ProductId
+    where F: Fn(usize) -> RangeInclusive<usize> {
+  let mut result = 0;
+  for range in ranges {
+    // For each possible number of pattern digits
+    for pat_digits in split_func(range.digits) {
+      // It must be an exact multiple of the full number of digits.
+      if range.digits.is_multiple_of(pat_digits) {
+        let expanded_mask = create_expanded_mask(POWER_10[pat_digits],
+                                                 range.digits / pat_digits);
+        let prefix_mask = POWER_10[range.digits - pat_digits];
+        for prefix in (range.range.start()/prefix_mask)..=(range.range.end()/prefix_mask) {
+          if (KEEP_DUPS || !has_repeats(prefix, pat_digits)) &&
+              range.range.contains(&(expanded_mask * prefix)) {
+            result += expanded_mask * prefix;
+          }
+        }
+      }
+    }
+  }
+  result
+}
+
+pub fn part1(ranges: &[RangeSlice]) -> ProductId {
+  // If digits is odd, the range is empty!
+  count_invalid::<_, true>(ranges, |digits| digits.div_ceil(2)..=(digits/2))
+}
+
+pub fn part2(ranges: &[RangeSlice]) -> ProductId {
+  count_invalid::<_, false>(ranges, |digits| 1..=(digits/2))
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{generator, part1, part2};
+
+  const INPUT: &str =
+"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,\
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,\
+824824821-824824827,2121212118-2121212124";
+
+  #[test]
+  fn test_part1() {
+    let data = generator(INPUT);
+    assert_eq!(1227775554, part1(&data));
+  }
+
+  #[test]
+  fn test_part2() {
+    let data = generator(INPUT);
+    assert_eq!(4174379265, part2(&data));
+  }
+}
